18ad7592827aa368fc2895b125e6690452a354bd
Lee, Giyeon <leessjjgg123456@naver.com>
2025-01-24 15:28:12 +0900

    mariadb

--- 07_grouping.sql
+++ 07_grouping.sql
@@ -42,3 +42,31 @@ SELECT
 	COUNT(*)
 	FROM tbl_menu
 	GROUP BY menu_price, category_code;
+	
+-- 카테고리별로 메뉴 가격 평균이 10000원 이상인 카테고리의 카테고리 코드, 카테고리명, 평균 메뉴 가격 조회
+SELECT a.category_code, b.category_name, AVG(a.menu_price)
+		FROM  tbl_menu a
+		JOIN tbl_category b ON a.category_code = b.category_code
+		GROUP BY a.category_code, b.category_name
+		HAVING AVG(a.menu_price) > 10000;
+		
+-- 앞 숫자는 내부 실행 순서, 작성 순서가 문법적 작성 순서
+-- 6 select : 조회 컬럼
+-- 1 from : 조회 대상 테이블
+-- 2 join : 조회 대상 테이블
+-- 3 where : 테이블 행을 조건으로 필터링
+-- 4 group by : 대상 컬럼으로 결과 집합 그룹핑
+-- 5 having : 그룹핑 결과를 조건으로 필터링
+-- 7 order by : 정렬 기준
+
+-- rollup : 중간 집계 함수로 group by와 함께 사용
+-- group by 절의 첫 번째 기준에 대한 중간 집계 + 총 집계 값이 result set에 포함된다.
+SELECT category_code, SUM(menu_price)
+	FROM tbl_menu
+	GROUP BY category_code
+	WITH ROLLUP;
+	
+SELECT menu_price, category_code, SUM(menu_price)
+	FROM tbl_menu
+	GROUP BY menu_price, category_code
+	WITH ROLLUP;
